756bfeeefb3152cb270df83ad10fce5b5ee572e4
muhammet-g <muhammetga642@gmail.com>
2024-05-22 16:33:32 +0300

    the procect done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./sass/custom.scss";
 import "./css/style.css";
 import "@fortawesome/fontawesome-free/js/all.min";
 import "/node_modules/sweetalert2/dist/sweetalert2.all.min";
+import "./sass/style.scss";
 
 document
   .querySelectorAll('[data-bs-toggle="tooltip"]')
--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
-        exclude: /bootstrap\.min\.css$/i,
+        test: /\.(sa|sc|c)ss$/i,
+        exclude: /custom\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
@@ -43,10 +43,11 @@ module.exports = {
             },
           },
           "css-loader",
+          "sass-loader",
         ],
       },
       {
-        test: /bootstrap\.min\.css$/i,
+        test: /custom\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
@@ -55,6 +56,7 @@ module.exports = {
             },
           },
           "rtlcss-loader",
+          "sass-loader",
         ],
       },
       {
